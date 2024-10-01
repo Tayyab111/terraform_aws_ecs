85a6c6b4ac30b762d553bed936c02ed541a11d2a
Tayyab111 <tayyabafridi843@gmail.com>
2024-10-01 18:35:12 +0500

    modify provider fie

--- provider.tf
+++ provider.tf
@@ -1,3 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+        version = "5.69.0"  # Update this version if needed
+    }
+  }
+}
+
 provider "aws" {
-  region = "us-east-1"
+  region     = "us-west-2"  # Change to your desired AWS region
+  #access_key = var.aws_access_key   # Use variables for sensitive data
+  #secret_key = var.aws_secret_key
 }
