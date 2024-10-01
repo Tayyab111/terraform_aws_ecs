7405bfd0b31159e372626ddfe20e5663c13fd8c3
Tayyab111 <tayyabafridi843@gmail.com>
2024-10-01 19:09:25 +0500

    regtion error

--- provider.tf
+++ provider.tf
@@ -2,13 +2,13 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-        version = "5.69.0"  # Update this version if needed
+        version = "~> 5.0"  # Update this version if needed
     }
   }
 }
 
 provider "aws" {
-  region     = "us-west-2"  # Change to your desired AWS region
+  region     = "us-east-1"  # Change to your desired AWS region
   #access_key = var.aws_access_key   # Use variables for sensitive data
   #secret_key = var.aws_secret_key
 }
